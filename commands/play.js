4b5d4c74cc9870e0209718443c34c00dbb976481
DanielRicklin <ricklin.daniel@outlook.fr>
2019-08-05 01:34:17 +0200

    sam saoul

--- commands/play.js
+++ commands/play.js
@@ -1,21 +1,22 @@
 const YoutubeStream = require('ytdl-core');
 
 exports.run = (client, message, args) => {
-	let voiceChannel = message.guild.channels.find(args[1]);
-    //   .filter(function (channel) { return channel.type === 'voice' })
-	//   .first();
+	// console.log(args);
+	let voiceChannel = message.guild.channels.find('name',args[1]);
+	// .filter(function (channel) { return channel.type === 'voice' })
+	// .first();
 	voiceChannel
-      .join()
-      .then(function (connection) {
-        let stream = YoutubeStream(args[0])
-        stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
-        connection
-          .playStream(stream)
-          .on('end', function () {
-            connection.disconnect()
-          })
-      })
+		.join()
+		.then(function (connection) {
+			let stream = YoutubeStream(args[0])
+			stream.on('error', function () {
+			message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+			connection.disconnect()
+			})
+			connection
+			.playStream(stream)
+			.on('end', function () {
+				connection.disconnect()
+			})
+		})
 }
