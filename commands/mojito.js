bade47523a7513c8f6f926603cdfa05c1deeea9e
danielricklin <ricklin.daniel@outlook.fr>
2019-08-10 11:33:47 +0200

    aezr

--- commands/mojito.js
+++ commands/mojito.js
@@ -12,7 +12,7 @@ exports.run = (client, message, args) => {
 			.catch(console.error);
 		//message.channel.send(args[0] + ' Voilà un mojito tout frais :tropical_drink:')
 	}*/
-	if(message.member.roles.find(r => r.name === "Rang 4") && args[0] != "<@607653983326306335>"){
+	if((message.member.roles.find(r => r.id === "526423192873599022") || message.member.roles.find(r => r.id === "526422194339708958")) && args[0] != "<@607653983326306335>"){
 		if(args[0]){
 			args.forEach(arg => {
 				message.channel.send(arg + ' Voilà un mojito tout frais :tropical_drink:')
