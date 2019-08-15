8d2bb4e5f8530e51e23ead574bd2f19d16b1e348
DanielRicklin <ricklin.daniel@outlook.fr>
2019-08-15 23:21:24 +0200

    aer

--- commands/ping.js
+++ commands/ping.js
@@ -2,5 +2,5 @@ exports.run = (client, message, args) => {
     if(message.author.username === 'laguerriere(dbb)')
         message.channel.send("Coucou moi du futur");
     else
-        message.channel.send("Coucou <@" + message.author.id + ">");
+        message.channel.send("azerazerCoucou <@" + message.author.id + ">");
 }
