a43893b0c30c15b9457591f8b8d7ec74f4a37e9c
Daniel Ricklin <ricklin.daniel@outlook.fr>
2020-03-12 12:52:26 +0100

    aze

--- commands/pr.js
+++ commands/pr.js
@@ -2,8 +2,6 @@ exports.run = async (client, message, args) => {
 
     const Rapport = require('../models/Rapport')
 
-    console.log(args.join(' '))
-
     if (message.attachments.size > 0) {
         await message.attachments.forEach(img => {
             try {
--- commands/rapport.js
+++ commands/rapport.js
@@ -2,9 +2,8 @@ exports.run = async (client, message, args) => {
 
     const Rapport = require('../models/Rapport')
 
-    // console.log(args.join(' '))
-
     const rapport = Rapport.find({ name: { $regex: '.*' + args.join(' ') + '.*' } }, function (err, rp) {
+        message.channel.send(rp.name);
         rp.forEach(img => {
             message.channel.send(img.url)
         });
